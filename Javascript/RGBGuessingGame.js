82e6aa0854fc37aab50c1cd8fe8cf5a01a5a3b0f
RYS Mosley <rysmosley@gmail.com>
2019-10-01 10:15:33 -0700

    Add files via upload

--- Javascript/RGBGuessingGame.js
+++ Javascript/RGBGuessingGame.js
@@ -1,9 +1,64 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1Header = document.getElementsByTagName("h1")[0];
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
+easyBtn.addEventListener("click", function() {
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+
+    for (var i = 0; i < squares.length; i++) {
+        if(colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+})
+
+hardBtn.addEventListener("click", function() {
+    easyBtn.classList.remove("selected");
+    hardBtn.classList.add("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+
+    for (var i = 0; i < squares.length; i++) {
+            squares[i].style.backgroundColor = colors[i];
+            squares[i].style.display = "block";
+    }
+})
+
+resetButton.addEventListener("click", function() {
+    // generate all new colors
+    colors = generateRandomColors(numSquares);
+
+    // pick a new random color from array
+    pickedColor = pickColor();
+
+    // change colorDisplay to match pickedColor
+    colorDisplay.textContent = pickedColor;
+
+    // change colors of squares
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.background = colors[i];
+    }
+
+    // reset background color of display
+    h1Header.style.backgroundColor = "steelblue";
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -17,8 +72,10 @@ for (var i = 0; i < squares.length; i++){
         var clickedColor = this.style.backgroundColor;
 
         // compare color to pickedColor
+        // check if won
         if(clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct!";
+            resetButton.textContent = "Play Again?";
             changeColors(clickedColor);
             h1Header.style.background = pickedColor;
         } else {
