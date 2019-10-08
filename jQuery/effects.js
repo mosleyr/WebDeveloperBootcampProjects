b4a1fe4b09c63d441f0200aabfe3af844afeb045
RYS Mosley <rysmosley@gmail.com>
2019-10-08 11:29:44 -0700

    Add files via upload

---
+++ jQuery/effects.js
@@ -0,0 +1,45 @@
+$('button').on("click", function() {
+    $('div').slideToggle(1000, function() {
+        console.log("Slide toggle completed.");
+    });
+});
+
+/*
+$('button').on("click", function() {
+    $('div').slideDown(1000, function() {
+        console.log("Slide down completed.");
+    });
+});
+
+$('button').on("click", function() {
+    $('div').slideUp(1000, function() {
+        console.log("Slide up completed.");
+    });
+});
+ */
+
+/*
+$('button').on("click", function() {
+    $('div').fadeToggle(1000, function() {
+        console.log("Fade completed.");
+    });
+});
+*/
+
+/*
+$('button').on("click", function() {
+    $('div').fadeIn(1000, function() {
+        console.log("Fade in completed.");
+    });
+});
+*/
+
+/*
+$('button').on("click", function() {
+    $('div').fadeOut(1000, function() {
+        console.log("Fade out completed.");
+        $(this).remove();
+    });
+});
+*/
+
